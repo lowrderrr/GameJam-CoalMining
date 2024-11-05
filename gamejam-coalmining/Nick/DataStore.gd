7368fa8650924bceca4027a2bb60e19eae72c3e3
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2024-11-04 19:31:39 -0600

    DataStore should be good to use!
    - DataStore.gd is global and can be accessed anywhere.
    - It's been tested and seems stable. Let me know if issues occur.
    - Redesigned functions so they're hopefully intuitive.
    - Details of the functions are in PR.

--- gamejam-coalmining/Nick/DataStore.gd
+++ gamejam-coalmining/Nick/DataStore.gd
@@ -2,8 +2,9 @@ extends TextEdit
 
 const SAVE_DIR = "user://saves/"
 const SAVE_FILE_NAME = "save.json"
-const SECURITY_KEY = "©89SADFH"
-const DEFAULT_ID = "test202410"
+#const SECURITY_KEY = "©89SADFH"
+const DEFAULT_DATA_SLOT = "test202410json"
+#const DEFAULT_DATA_SLOT = "test202410"
 
 #var player_data = PlayerData.new()
 #var player_data = {}
@@ -16,71 +17,102 @@ func get_file(path, access = FileAccess.READ):
 	if path == null:
 		print("get_file(): 1st argument `path` is empty.")
 		return
-		
+
 	#return FileAccess.open_compressed(path, access) # not effective. if there's more data, maybe
 	return FileAccess.open(path, access)
 	#return FileAccess.open_encrypted_with_pass(path, access, SECURITY_KEY)
 
 # Returns false if data could not be saved
-func save_data(new_data, data_slot: String = DEFAULT_ID) -> bool:
-	print('EXPERIMENTAL feature. May break.') 
-	print("Saved: " + SAVE_DIR + data_slot)
+func save_data(data, data_slot: String = DEFAULT_DATA_SLOT) -> bool:		
+	#var json_string = JSON.stringify(data, "\t") #not recommended unless converting godot to json
+	var json_string = JSON.parse_string(data)
+	if json_string == null:
+		print("This data can't be parsed to json. save_data() has rejected new saved data to prevent corruption.")
+		return false
+	
 	var file = get_file(SAVE_DIR + data_slot, FileAccess.WRITE)
 	if file == null:
 		print(FileAccess.get_open_error())
 		return false
-
-	#player_data = {
-		#"player_data":{
-			#"health": player_data.health,
-			#"position":{
-				#"x": player_data.global_position.x,
-				#"y": player_data.global_position.y
-			#},
-			#"coins": player_data.coins
-		#}
-	#}
 	
-	#var json_string = JSON.stringify(data, "\t")
 	#file.store_string(json_string)
-	file.store_string(new_data)
+	file.store_string(data)
 	file.close()
+	print("Saved: " + SAVE_DIR + data_slot)
 	return true
 
 
-func load_data(data_slot: String = DEFAULT_ID): # return change later?
-	print('EXPERIMENTAL feature. May break.')
+func _get_init_data():
+	# This changes the default data.
+	return {
+		"data_ver":-99, # If data is incompatible with pervious data, might want to increment this number
+		#"hp": 100,
+		"pos":{
+			"x": -1,
+			"y": -1
+		},
+		#"item_pos":{
+			###cleaneroption
+			#{
+				#"name": "coal",
+				#"x": -1,
+				#"y": -1
+			#},
+			###spaceefficientoption
+			#"coal":{
+				#[0]:{
+					#"x": -1,
+					#"y": -1
+				#},
+				#[1]:{
+					#"x"
+				#}
+			#}
+		#},
+		"inventory":{
+			"coal": 5,
+			"coins": 0
+		},
+		#"terrain":2323 #seed??
+	}
+	
+
+# assume an error if it's not json
+func load_data(data_slot: String = DEFAULT_DATA_SLOT):
 	var path = SAVE_DIR + data_slot
 	print(path)
+	
 	if FileAccess.file_exists(path):
 		var file = get_file(path)
 		if file == null:
 			print("Could not load data from " + str(path) + ". Possible error message below:")
 			print(FileAccess.get_open_error())
-			return
+			return "Error: CannotOpenDataFile"
 
 		var data = file.get_as_text()
+		# This code assume that data is not corrupted btw
+			
 		print(str(data) + " is real and won't hurt u")
 		file.close()
 
-		#var data = JSON.parse_string(content)
-		if data == null:
+		var json_data = JSON.parse_string(data)
+		
+		if json_data == null:
 			#printerr("Cannot parse %s as a json_string: (%s)" % [data_slot, content])
-			printerr("Cannot parse %s: (%s)" % [data_slot, data])
-			return "gameerror loading data"
+			printerr("Cannot load data as json from data slot %s: (%s)" % [data_slot, data])
+			return "Error: DataFileNotJson"
 		else:
 			print("Loaded: " + str(data))
 			return data
+	else:
+		return _get_init_data()
 
 		# init data + defaults
 
-func del_data(data_slot): # Dangerous! no DEFAULT ID for safe measures ... can be changed later.
+func del_data(data_slot): # Dangerous! no DEFAULT ID for safe measures ... can be changed later if desired.
 	#$".".clear() # this will fire .changed() so note two file writes may occur
 	print("Deleting!\n")
 	DirAccess.remove_absolute(SAVE_DIR + data_slot)
-
-	print("Reloading (after 1 second delay)")
-	return load_data(data_slot)
 
 # example functions
 
@@ -88,18 +120,20 @@ func _on_text_changed() -> void: # save data
 	print("\nsaving text:")
 	var txt = $".".text
 	print(txt)
-	if save_data(txt):
+	if DataStore.save_data(txt):
 		print("saved data!")
 	else:
 		print("failed to save data!")
 	#save_data(txt, "test202410")
 
 func _on_button_pressed() -> void:
-	$".".text = str(del_data("test202410"))
+	DataStore.del_data("test202410")
+	$".".text = str(load_data())
 
 	print("no wayy")
 
 func _ready(): # load data
 	verify_save_folder(SAVE_DIR)
 	#load_data("SAVE_DIR + SAVE_FILE_NAMErobux")
-	$".".text = str(load_data()) + $".".text
+	var data = DataStore.load_data()
+	$".".text = str(data) + $".".text

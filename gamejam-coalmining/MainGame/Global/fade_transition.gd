eb93e829dbe5d66fa39483d508014abd93cb979e
FrancisTR <fatran@stthomas.edu>
2024-11-05 19:07:00 -0600

    Added fade transitions and new assets

---
+++ gamejam-coalmining/MainGame/Global/fade_transition.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+# To use it, use the code below:
+# FadeTransition.transition()
+# await FadeTransition.on_transition_finished
+
+signal on_transition_finished
+
+@onready var color_rect = $ColorRect
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	color_rect.visible = false
+	animation_player.animation_finished.connect(_on_animation_finished)
+	
+func _on_animation_finished(anim_name):
+	if anim_name == "fade_to_black":
+		on_transition_finished.emit()
+		animation_player.play("fade_to_normal")
+	elif anim_name == "fade_to_normal":
+		color_rect.visible = false
+
+func transition():
+	color_rect.visible = true
+	animation_player.play("fade_to_black")
